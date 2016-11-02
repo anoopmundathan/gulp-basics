3cfab138bd603ab4190372f7d9a41c0324a3030c
anoopmundathan <anoop.mundathan@gmail.com>
2016-11-02 12:07:42 +0000

    Add gulp-sass plugin

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@ var gulp = require('gulp');
 var concat = require('gulp-concat');
 var rename = require('gulp-rename');
 var uglify = require('gulp-uglify');
+var sass = require('gulp-sass');
 
 gulp.task('scripts', function() {
   gulp.src('js/*.js')
@@ -12,6 +13,12 @@ gulp.task('scripts', function() {
     .pipe(gulp.dest('dist/'));
 });
 
-gulp.task('default', ['scripts'], function() {
+gulp.task('sass', function() {
+  gulp.src('scss/*.scss')
+    .pipe(sass())
+    .pipe(gulp.dest('css/'));
+});
+
+gulp.task('default', ['scripts','sass'], function() {
   console.log('all tasks completed');
 });
