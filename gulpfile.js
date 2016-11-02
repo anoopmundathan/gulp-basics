436a5cdf14f2a0fc1c85c53ca6ec2e3b6609f234
anoopmundathan <anoop.mundathan@gmail.com>
2016-11-02 06:42:24 +0000

    Added gulp-concat module

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,11 @@
 var gulp = require('gulp');
+var concat = require('gulp-concat');
+
+gulp.task('scripts', function() {
+  gulp.src('./js/*.js')
+    .pipe(concat('all.js'))
+    .pipe(gulp.dest('./dist/'));
+});
 
 gulp.task('task1', function() {
   console.log('gulp task1 completed');
@@ -8,6 +15,6 @@ gulp.task('task2', function() {
   console.log('gulp task2 completed');
 });
 
-gulp.task('default', ['task1','task2'], function() {
+gulp.task('default', ['task1','task2','scripts'], function() {
   console.log('all tasks completed');
 });
