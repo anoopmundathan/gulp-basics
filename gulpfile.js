f09199983e563d830ffeda2d71273cffe7e4ac42
anoopmundathan <anoop.mundathan@gmail.com>
2016-11-03 07:06:45 +0000

    Add del module and build pipeline

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,7 @@ var rename = require('gulp-rename');
 var uglify = require('gulp-uglify');
 var sass = require('gulp-sass');
 var maps = require('gulp-sourcemaps');
+var del = require('del');
 
 // 1. Concatinate js files and create final.js in dist folder
 gulp.task('concat', function() {
@@ -16,7 +17,7 @@ gulp.task('concat', function() {
 
 // 2. Minify js files
 gulp.task('uglify', ['concat'], function() {
-  gulp.src('js/app.js')
+  return gulp.src('js/app.js')
     .pipe(uglify())
     .pipe(rename('app.min.js'))
     .pipe(gulp.dest('js'));
@@ -24,7 +25,7 @@ gulp.task('uglify', ['concat'], function() {
 
 // 3. Compile sass file into css
 gulp.task('sass', function() {
-  gulp.src('scss/*.scss')
+  return gulp.src('scss/*.scss')
     .pipe(maps.init())
     .pipe(sass())
     .pipe(maps.write('/'))
@@ -37,7 +38,17 @@ gulp.task('watchSass', function() {
 });
 
 // Build task
-gulp.task('build', ['uglify', 'sass']);
+gulp.task('build', ['uglify', 'sass'], function() {
+  return gulp.src(['js/app.min.js', 'css/style.css'], {base:'./'})
+    .pipe(gulp.dest('dist'));
+});
+
+// Clean task before build
+gulp.task('clean', function() {
+  return del(['dist','js/app*.js*','css']);
+});
 
 // Default gulp task
-gulp.task('default',['build']);
+gulp.task('default', ['clean'], function() {
+  gulp.start('build');
+});
